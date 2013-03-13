77a6dbacf5955c6a7be08d4714059cdde023b735
Troy <troydl@mail.fresnostate.edu>
2013-03-13 12:24:09 -0700

    added code to find D

--- lab7/lab7.cc
+++ lab7/lab7.cc
@@ -1,6 +1,6 @@
 //
-//  STUDENT NAME
-//  STUDENT ID
+//  Troy Lonie
+//  103537249
 //  CSci 115
 //  Lab 7
 //
@@ -8,8 +8,23 @@
 //
 
 #include "lab7.h"
+#include <iostream>
 
+void counting_sort(unsigned int * A, unsigned int * B, int K); //prototype
+
+//'r' bits for each run.  Check with r = 4, 8, 16.  Use counting sort.
 void LSD_radixSort(unsigned int * A, int size, int r)
+{
+  int D = (A[1].sizeof() * 8) / r;
+  cout << "D = " << D << endl;
+  for( int i = 0; i < D; i++ )
+  {
+
+  }
+}
+
+
+void counting_sort(unsigned int * A, unsigned int * B, int K)
 {
 
 }
