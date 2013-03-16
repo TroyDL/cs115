302027af5daf8168fcc2c1dd481a60f1da880720
Troy Lonie <troydl@mail.fresnostate.edu>
2013-03-16 15:37:49 -0700

    Added current lab7.cc to uwork on offline

--- lab7/lab7.cc
+++ lab7/lab7.cc
@@ -9,13 +9,13 @@
 
 #include "lab7.h"
 #include <iostream>
-
+using namespace std;
 void counting_sort(unsigned int * A, unsigned int * B, int K); //prototype
 
 //'r' bits for each run.  Check with r = 4, 8, 16.  Use counting sort.
 void LSD_radixSort(unsigned int * A, int size, int r)
 {
-  int D = (A[1].sizeof() * 8) / r;
+  int D = (sizeof(unsigned int) * 8) / r;
   cout << "D = " << D << endl;
   for( int i = 0; i < D; i++ )
   {
